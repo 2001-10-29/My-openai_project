13a185f9c87dcafc29515fa25e825d35efa08128
unknown <4030079@myuwc.ac.za>
2026-03-26 22:40:30 +0200

    add

---
+++ Multi-turn conversations with GPT/Creating an AI chatbot.py
@@ -0,0 +1,35 @@
+import os
+from xmlrpc import client 
+from dotenv import load_dotenv
+from openai import OpenAI 
+
+#load environment variables from .env
+load_dotenv() 
+
+# Get API key
+api_key = os.getenv("OPENAI_API_KEY")
+
+#Create OpenaAI client 
+client = OpenAI(api_key=api_key)
+messages = [{"role": "system", "content": "You are a helpful math tutor that speaks concisely."}]
+user_msgs = ["Explain what pi is.", "Summarize this in two bullet points."]
+
+# Loop over the user questions
+for q in user_msgs:
+    print("User: ", q)
+    
+    # Create a dictionary for the user message from q and append to messages
+    user_dict = {"role": "user", "content": q}
+    messages.append(user_dict)
+    
+    # Create the API request
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        max_completion_tokens=100
+    )
+    
+    # Append the assistant's message to messages
+    assistant_dict = {"role": "assistant", "content": response.choices[0].message.content}
+    messages.append(assistant_dict)
+    print("Assistant: ", response.choices[0].message.content, "\n")
